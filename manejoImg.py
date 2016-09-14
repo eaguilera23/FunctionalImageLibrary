049155c845f55c06a0edcef28c87a5689ad24eaf
Eduardo Aguilera <laloao.2302@gmail.com>
2016-09-14 11:12:18 -0500

    asking for revision

--- manejoImg.py
+++ manejoImg.py
@@ -8,10 +8,32 @@ import random
 
 OUTPUT_FILE = 'output2.png'
 
-def det():
-  return random.randint(0,1)
+# INCOMPLETO
+def blur(image, n):
+  w, y = image.size
+  pix = img.load()
+  output = Image.new("RGB", (w,h))
+
+  # me regresa el pixel ya modificado
+  def new_pixel(array):
+    return array[0]/5, array[1]/5, array[2]/5
+
+  # returns a list of lists
+  # each list is a pixel
+  five_pixels = lambda i,j: [ list(pix[i,j]), list(pix[i-1,j]), list(pix[i+1,j]), list(pix[i,j+1]), list(pix[i,j-1]) ]
+  # Esto es igual al original
+  col_totals = lambda i,j: [ sum(x) for x in zip(*five_pixels(i)(j)) ]
+
+  # Esto me genera mi arreglo que se convertirá en la imagen
+  # Necesitamos procesar este array varias veces
+  pre_out = [ [ new_pixel(col_totals(i)(j)) for j in range(h) ] for i in range(w) ]
+  output.putdata(out_pix)
+  output.save(OUTPUL_FILE, 'PNG')
+  return output
 
 def sal_y_pimienta(img, intensidad, pol):
+  def det():
+    return random.randint(0,1)
   w, h = img.size
   pix = list(img.getdata())
   total_size = w*h
